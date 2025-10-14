3e3d4a20c131da0ca6f0b713947edda8567d97ab
nqtienvn <quangtiencoder1876@gmail.com>
2025-10-14 10:06:15 +0700

    demo

---
+++ src/main/java/com/tien/iam_service2_keycloak/IamService2KeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.tien.iam_service2_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamService2KeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamService2KeycloakApplication.class, args);
+    }
+
+}
